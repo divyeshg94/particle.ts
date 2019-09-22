3185559a4a31360fcdd2602aaac19f5706db6d9a
DIVYESH\divye <divyeshg@cloudassert.com>
2019-09-22 18:56:57 +0530

    particle

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ParticlesConfig } from 'src/shared/particle-config';
+
+declare let particlesJS: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'DBlog';
+
+  ngOnInit(): void {
+    this.invokeParticles();
+  }
+
+  public invokeParticles(): void {
+    particlesJS('particles-js', ParticlesConfig, function() {});
+  }
+}
